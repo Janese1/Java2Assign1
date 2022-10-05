c60bca53cf4907f5345d4cbc5c8b875b00c99ba0
Janese <a2004z@qq.com>
2022-10-05 17:11:22 +0800

    Change1

--- A1_Sample/src/Movie.java
+++ A1_Sample/src/Movie.java
@@ -8,16 +8,24 @@ public class Movie {
     private String overview;
     private String score;
     private String director;
-    private String stars;
+    private String star1;
+    private String star2;
+    private String star3;
+    private String star4;
     private String noofvotes;
     private String gross;
+
+    public String getGenre() {
+        return genre;
+    }
 
     public int getYear() {
         return year;
     }
 
     public Movie(String title, int year, String certificate, String runtime, String genre, String rating,
-                 String overview, String score, String director, String stars, String noofvotes, String gross){
+                 String overview, String score, String director, String star1,String star2,String star3,
+                 String star4,String noofvotes, String gross){
         this.title=title;
         this.year=year;
         this.certificate=certificate;
@@ -27,7 +35,10 @@ public class Movie {
         this.overview=overview;
         this.score=score;
         this.director=director;
-        this.stars=stars;
+        this.star1=star1;
+        this.star2=star2;
+        this.star3=star3;
+        this.star4=star4;
         this.noofvotes=noofvotes;
         this.gross=gross;
     }
--- A1_Sample/src/MovieAnalyzer.java
+++ A1_Sample/src/MovieAnalyzer.java
@@ -1,10 +1,6 @@
-import javax.xml.transform.Result;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class MovieAnalyzer {
@@ -17,12 +13,23 @@ public class MovieAnalyzer {
         ArrayList<Movie> list = new ArrayList<>();
         while ((movie = reader.readLine()) != null) {
             String[] strArray = movie.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+
+            for (int i = 0; i < strArray.length; i++) {
+                if (strArray[i].contains("\"")){
+                    strArray[i]=strArray[i].substring(1,strArray[i].length()-1);
+                }
+            }
+
             if (strArray[2].equals("")) {
-                list.add(new Movie(strArray[1], 0, strArray[3], strArray[4], strArray[5], strArray[6], strArray[7],
-                        strArray[8], strArray[9], strArray[10], strArray[11], strArray[12]));
+                list.add(new Movie(strArray[1], 0, strArray[3], strArray[4],
+                        strArray[5], strArray[6], strArray[7],
+                        strArray[8], strArray[9], strArray[10], strArray[11], strArray[12],strArray[13],
+                        strArray[14],strArray[15]));
             } else {
-                list.add(new Movie(strArray[1], Integer.parseInt(strArray[2]), strArray[3], strArray[4], strArray[5], strArray[6], strArray[7],
-                        strArray[8], strArray[9], strArray[10], strArray[11], strArray[12]));
+                list.add(new Movie(strArray[1], Integer.parseInt(strArray[2]), strArray[3], strArray[4],
+                        strArray[5], strArray[6], strArray[7],
+                        strArray[8], strArray[9], strArray[10], strArray[11], strArray[12],strArray[13],
+                        strArray[14],strArray[15]));
             }
         }
         this.list=new ArrayList<>();
@@ -37,17 +44,41 @@ public class MovieAnalyzer {
                 .entrySet().stream()
                 .sorted(Map.Entry.<Integer,Integer>comparingByKey().reversed())
                 .forEachOrdered(x -> result.put(x.getKey(), x.getValue()));
-                System.out.println(result);
+               // System.out.println(result);
                 return result;
     }
 
     public Map<String, Integer> getMovieCountByGenre(){
-        
+        ArrayList<String> genre =new ArrayList<>();
+        for (Movie i:
+             list) {
+            if (i.getGenre().contains(",")){
+                String[] array=i.getGenre().split(",");
+                for (String e:
+                     array) {
+                    genre.add(e.replaceAll(" ",""));
+                }
+            } else{
+                genre.add(i.getGenre());
+            }
+        }
+        Map<String,Integer> result=new LinkedHashMap<>();
+       genre.stream().collect(Collectors.toMap(p->p,s->1,Integer::sum))
+               .entrySet().stream()
+               .sorted(Map.Entry.<String,Integer>comparingByValue().reversed()
+                       .thenComparing(Map.Entry.comparingByKey()))
+               .forEachOrdered(x -> result.put(x.getKey(), x.getValue()));
+        //System.out.println(result);
+        return result;
     }
 
+    public Map<List<String>, Integer> getCoStarCount(){
+
+        return null;
+    }
     public static void main(String[] args) throws IOException {
         MovieAnalyzer a = new MovieAnalyzer("A1_Sample/resources/imdb_top_500.csv");
-        Map<Integer, Integer> b = a.getMovieCountByYear();
+        Map<String, Integer> b = a.getMovieCountByGenre();
 
     }
 }
